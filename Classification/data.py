4178b1f0e567c9145d48da00263d7f0d400d6bd5
lgomesgl <lgomes855@gmail.com>
2023-06-20 21:37:11 -0300

    loop_in_kernel

--- Classification/data.py
+++ Classification/data.py
@@ -6,17 +6,17 @@ def create_dataframes(columns):
     return pd.DataFrame(columns=columns)
 
 def save_the_data(data, name_csv_file):
-    return data.to_csv('Results' % name_csv_file, index=True)
+    return data.to_csv(name_csv_file, index=True)
 
 def exclude_the_data(folder_path, name_csv_file):
     if os.path.isfile(folder_path):
-        os.remove('%s\%s' %(folder_path, name_csv_file))
+        os.remove('%s\%s' % (folder_path, name_csv_file))
         
 def separate_the_data_by_column(data, column):
     values = data[column].unique()
     dataframes = {}
     for i in range(len(values)):
-        dataframes['df_%s' % str(i+1)] = data[(data[column] == values[i])]
+        dataframes['df_%s' % str(values[i])] = data[(data[column] == values[i])]
     return dataframes
 
 def row_to_append(dataframe, columns, values):

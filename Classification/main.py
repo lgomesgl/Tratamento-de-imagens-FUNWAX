e4c7df002c8aae3f331c308c3bae6c1cfb8f7776
lgomesgl <lgomes855@gmail.com>
2023-07-05 10:00:37 -0300

    att

--- Classification/main.py
+++ Classification/main.py
@@ -5,8 +5,8 @@ from data import create_dataframes, row_to_append, separate_the_data_by_column, 
 from classification_crystals import get_properties, get_image, crop_the_image, filter, classification
 from pos_processing import graphics
 
-# FOLDER_PATH = '/home/lucas/FUNWAX/Images' ## linux path
-FOLDER_PATH = 'D:\LUCAS\IC\FUNWAX\Images'
+FOLDER_PATH = '/home/lucas/FUNWAX/Images' ## linux path
+# FOLDER_PATH = 'D:\LUCAS\IC\FUNWAX\Images'
 NAME_CSV_FILE = 'Results.csv'
 
 data = create_dataframes(['Type', 'Reynolds', 'Toil', 'Tcool', 'Time', 'cx', 'cy', 'major', 'minor', 'angle', 'kernel', 'AR'])
@@ -31,9 +31,9 @@ for i, file in enumerate(files):
         contours = filter(image, properties, kernel)
         data, image, contours, n_of_crystals, cnt_ellipse, cnt_rect = classification(image, data, contours, properties, cnt_ellipse, cnt_rect, kernel)
 
-    n_of_crystals_.append(n_of_crystals)
-    row_to_append = pd.DataFrame([{'Type':properties[1], 'Reynolds':properties[3], 'Toil':properties[4], 'Tcool':properties[5], 'Time':properties[6], 'N_of_crystals': int(n_of_crystals-n_of_crystals_[i])}])
-    data_crystals = pd.concat([data_crystals, row_to_append], ignore_index=True)
+    # n_of_crystals_.append(n_of_crystals)
+    # row_to_append = pd.DataFrame([{'Type':properties[1], 'Reynolds':properties[3], 'Toil':properties[4], 'Tcool':properties[5], 'Time':properties[6], 'N_of_crystals': int(n_of_crystals-n_of_crystals_[i])}])
+    # data_crystals = pd.concat([data_crystals, row_to_append], ignore_index=True)
     
     # data_crystals = row_to_append(data_crystals, ['Type', 'Reynolds', 'Toil', 'Tcool', 'Time', 'N_of_crystals'],
     #                               [properties[1], properties[3], properties[4], properties[5], properties[6], (n_of_crystals - n_of_crystals_[i])])
@@ -49,8 +49,8 @@ dataframes = separate_the_data_by_column(data, 'kernel')
 '''
 
 # print(data)
-graphics(dataframes['df_(1, 1)'], data_crystals)
-graphics(dataframes['df_(3, 3)'], data_crystals)
+graphics(dataframes['df_(1, 1)'])
+graphics(dataframes['df_(3, 3)'])
 
 # print('AR calculate by ellipse: %s' % cnt_ellipse)
 # print('AR calculate by rectangle: %s' % cnt_rect)
